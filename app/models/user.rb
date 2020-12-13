de494a3ecf0fccfc67940e1e5a09feb998f78006
”Shin <shin02060804@gmail.con>
2020-12-13 13:35:59 +0000

    s

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    
+    validates :name, presence: true, length: {maximum: 15}
+    validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, length: {minimum: 8, maximum: 32 }
+    
+    has_secure_password
 end
